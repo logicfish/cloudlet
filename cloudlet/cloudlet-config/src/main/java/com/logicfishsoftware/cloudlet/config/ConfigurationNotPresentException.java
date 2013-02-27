1bb20c4a401f3cd0a34d3967aff2402a9937351b
Mark Fisher <logicfish@gmail.com>
2013-02-27 00:18:34 +0000

    missing file

---
+++ cloudlet/cloudlet-config/src/main/java/com/logicfishsoftware/cloudlet/config/ConfigurationNotPresentException.java
@@ -0,0 +1,35 @@
+package com.logicfishsoftware.cloudlet.config;
+
+public class ConfigurationNotPresentException extends ConfigurationException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ConfigurationNotPresentException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConfigurationNotPresentException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConfigurationNotPresentException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConfigurationNotPresentException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConfigurationNotPresentException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
